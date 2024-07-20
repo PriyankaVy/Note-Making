8e5f2d0158b82b213e12d70d9e2c62a60fe32120
PriyankaVy <pvysyaraju@seattleu.edu>
2024-07-19 17:37:42 -0700

    changed the redirect path

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -21,10 +21,11 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path = "/" element = {<ProtectedRoute><Home /></ProtectedRoute>}/> 
+        <Route path = "/home" element = {<ProtectedRoute><Home /></ProtectedRoute>}/> 
         <Route path = "/login" element = {<form route="/api/token/" method="login" />}/>
         <Route path = "/logout" element = {<Logout />}/>
         <Route path = "/register" element = {<form route="/api/user/register/" method="register" />}/>
+        <Route path = "/" element = {<Navigate to="/login" />}/>
         <Route path= "*" element = {<NotFound />}/>
       </Routes>  
     </BrowserRouter>
